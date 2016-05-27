6063aaf49b5ad8d057e7a41ca9c509f467939f27
lwin khine mon <lkm.tokyo@gmail.com>
2016-05-27 13:30:58 +0900

    may 25 first

--- 02/result.php
+++ 02/result.php
@@ -166,8 +166,14 @@ print "その他";
 <!--文字をstyle入れる為divを入れる-->
 <div class="done">
 送信しました。
-</div>
+</div><br><br>
 </p>
+
+<!--元ページに戻って書き直す為、元のページにリンク-->
+<p>
+<div class="rewrite">
+<a href="contact.php">元のページに戻ります</a>
+</div>
 
 </div>
 </body>
